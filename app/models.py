81a7a1f495c6292279b07a9cb989af4d0dd0f6fb
barackmaund1 <pmaundu49@gmail.co>
2020-05-08 07:12:55 +0300

    created writers navbar

--- app/models.py
+++ app/models.py
@@ -14,7 +14,7 @@ class User(db.Model,UserMixin):
     name = db.Column(db.String(80), nullable=False)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(200),  nullable=False)
+    pass_secure = db.Column(db.String(200),  nullable=False)
     profile = db.Column(db.String(180),  default="profile.jpg")
     post=db.relationship('Post',backref='user',lazy='dynamic')
     @property
@@ -38,13 +38,11 @@ class Post(db.Model):
     title = db.Column(db.String(200), unique=True, nullable=False)
     slug = db.Column(db.String(200), unique=True, nullable=False)
     body = db.Column(db.Text, nullable=False)
-    category = db.Column(db.String(100), nullable=False)
     image = db.Column(db.String(150), nullable=False, default='no-image.jpg')
     user_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
     author = db.relationship('User', backref=db.backref('posts',lazy=True, passive_deletes=True))
     views = db.Column(db.Integer,default=0)
     comments = db.Column(db.Integer,default=0)
-    feature = db.Column(db.String, default=1, nullable=False)
     date_pub = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     def __repr__(self):
@@ -61,10 +59,7 @@ class Comments(db.Model):
      __tablename__='comments'
      
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(200), unique=False, nullable=False)
-    email = db.Column(db.String(200), unique=False, nullable=False)
-    message = db.Column(db.Text, nullable=False)
+    comment= db.Column(db.Text, nullable=False)
     post_id = db.Column(db.Integer, db.ForeignKey('posts.id', ondelete='CASCADE'), nullable=False)
     post = db.relationship('Post', backref=db.backref('posts',lazy=True, passive_deletes=True))
-    feature = db.Column(db.Boolean, default=False, nullable=False)
     date_pub = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)          
